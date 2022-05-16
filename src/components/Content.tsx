6d99a6070d93f272dd89d2ca62d85f7264c004a6
Matheus Rian <souzamatheusrian@gmail.com>
2022-05-15 22:01:58 -0300

    feat: Add component Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,68 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+import { SideBar } from "./SideBar";
+
+import '../styles/content.scss';
+import { Header } from "./Header";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function Content() {
-  // Complete aqui
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <>
+      <SideBar handleGenre={handleClickButton} selectedGenre={selectedGenreId} />
+
+      <div className="container">
+        <Header title={selectedGenre.title} />
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard 
+                key={movie.imdbID} 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} 
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
