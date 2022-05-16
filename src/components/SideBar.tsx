3a9042d29c610f92c2860718b9c8a092d6b7d2b0
Matheus Rian <souzamatheusrian@gmail.com>
2022-05-15 22:02:44 -0300

    feat: Add component Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
+import { useState, useEffect } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  handleGenre: (id: number) => void;
+  selectedGenre: number;
+}
+
+export function SideBar({ handleGenre, selectedGenre }: SideBarProps) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleGenre(genre.id)}
+            selected={selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
